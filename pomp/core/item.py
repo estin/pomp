bde750f09dad757ba180a00cb8413bf098ed58d8
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2013-04-18 17:16:53 +0400

    fix Item field ordering

--- pomp/core/item.py
+++ pomp/core/item.py
@@ -14,10 +14,15 @@ class Item(OrderedDict):
         # Populate ordered dict
         _args = list(args[:])
         _args.reverse()
+        fields_by_creation_counter = {}
         for field, obj in inspect.getmembers(self.__class__):
             if isinstance(obj, Field):
-                value = _args.pop() if _args else kwargs.get(field, None)
-                super(Item, self).__setitem__(field, value)
+                fields_by_creation_counter[obj.counter] = field
+
+        for key in sorted(fields_by_creation_counter):
+            field = fields_by_creation_counter[key]
+            value = _args.pop() if _args else kwargs.get(field, None)
+            super(Item, self).__setitem__(field, value)
 
     def __setattr__(self, key, value):
         if key in self:
@@ -26,4 +31,8 @@ class Item(OrderedDict):
 
 
 class Field(object):
-    pass
+    counter = 0
+
+    def __init__(self):
+        self.counter = Field.counter
+        Field.counter += 1

6925a22fd549fe54208769451dedb760078d841b
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2015-10-31 22:45:59 +0300

    typo

--- drone.sh
+++ drone.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# install python3.5
 if ! type python3.5 > /dev/null; then
     sudo add-apt-repository ppa:fkrull/deadsnakes > /dev/null 2>&1
     sudo apt-get -qq update > /dev/null 2>&1
-    sudo apt-get -qq install python3.5-dev > /dev/null 2>&1kkk
+    sudo apt-get -qq install python3.5-dev > /dev/null 2>&1
 fi
 
 # drone.io run script

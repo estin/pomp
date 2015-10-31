d5b024ada2ebe515923dd9b6b71e30638002e89e
Evgeniy Tatarkin <tatarkin.evg@gmail.com>
2015-10-31 22:43:45 +0300

    update tox and dron configs

--- drone.sh
+++ drone.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# install python3.5
+if ! type python3.5 > /dev/null; then
+    sudo add-apt-repository ppa:fkrull/deadsnakes > /dev/null 2>&1
+    sudo apt-get -qq update > /dev/null 2>&1
+    sudo apt-get -qq install python3.5-dev > /dev/null 2>&1kkk
+fi
+
 # drone.io run script
 pip install tox
-tox -e py27,py33,docs,py27examples,py33examples,qa
+tox -e py27,py35,docs,py27examples,py35examples,qa
